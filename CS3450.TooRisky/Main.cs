4803b443d83b029ce9c970d2afd740749baf1a0d
Ronin11 <nate@raddd.com>
2015-11-06 10:19:23 -0700

    Added XML and files

---
+++ CS3450.TooRisky/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace CS3450.TooRisky
+class Main
+{
+    static void Main()
+    {
+
+    }
+}
---
+++ CS3450.TooRisky/XML.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CS3450.TooRisky
+{
+    static class XML
+    {
+        public void read()
+        {
+            // Create an XML reader for this file.
+            using (XmlReader reader = XmlReader.Create("perls.xml"))
+            {
+                while (reader.Read())
+                {
+                    // Only detect start elements.
+                    if (reader.IsStartElement())
+                    {
+                        // Get element name and switch on it.
+                        switch (reader.Name)
+                        {
+                            case "perls":
+                                // Detect this element.
+                                Console.WriteLine("Start <perls> element.");
+                                break;
+                            case "article":
+                                // Detect this article element.
+                                Console.WriteLine("Start <article> element.");
+                                // Search for the attribute name on this current node.
+                                string attribute = reader["name"];
+                                if (attribute != null)
+                                {
+                                    Console.WriteLine("  Has attribute name: " + attribute);
+                                }
+                                // Next read will contain text.
+                                if (reader.Read())
+                                {
+                                    Console.WriteLine("  Text node: " + reader.Value.Trim());
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+    }
+}
