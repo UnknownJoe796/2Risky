f463df6d1e49a971dc59cc07aa33cfeb9f6e40bc
Fabio Gottlicher <g.fabio.cz@gmail.com>
2015-12-05 22:26:17 -0700

    adds splashscreen - closes #25

---
+++ CS3450.TooRisky/Utils/GameLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.Authentication.OnlineId;
+
+namespace CS3450.TooRisky.Utils
+{
+    public static class GameLog
+    {
+        public static List<string> Events = new List<string>();
+
+        public static void AddEvent(string description)
+        {
+            var now = DateTime.Now.ToString("h:mm:ss");
+            Events.Add(now + ": " + description);
+        }
+    }
+}
