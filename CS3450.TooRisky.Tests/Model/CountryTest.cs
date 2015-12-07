6159e5a7821ff10f837fad73549766d94de57ce9
Fabio Gottlicher <g.fabio.cz@gmail.com>
2015-12-07 13:53:37 -0700

    fixed test project

---
+++ CS3450.TooRisky.Tests/Model/CountryTest.cs
@@ -0,0 +1,101 @@
+﻿using CS3450.TooRisky.Model;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS3450.TooRisky.Tests.Model
+{
+    [TestClass]
+    public class CountryTest
+    {
+
+        [TestMethod]
+        public void OwnedBy()
+        {
+            Game game = new GameTest().MakeExampleGame();
+            Assert.AreEqual(
+                game.Countries["Testistan"].OwnedBy,
+                PlayerNumber.P1
+                , "Ownership test 1 failed");
+            Assert.AreEqual(
+                game.Countries["Testanbul"].OwnedBy,
+                PlayerNumber.P2
+                , "Ownership test 2 failed");
+        }
+
+        [TestMethod]
+        public void AdjacentCountries()
+        {
+            Game game = new GameTest().MakeExampleGame();
+            var owned = game.Countries["Testistan"].AdjacentCountries();
+            Assert.IsTrue(owned.Contains(game.Countries["Testlyvania"]));
+            Assert.IsTrue(owned.Contains(game.Countries["Testanbul"]));
+            Assert.IsFalse(owned.Contains(game.Countries["Test States of America"]));
+        }
+
+        [TestMethod]
+        public void Attacks()
+        {
+            Game game = new GameTest().MakeExampleGame();
+            var actions = game.Countries["Testistan"].Attacks();
+            Assert.IsTrue(actions.Contains(new Attack()
+            {
+                PlayerNumber = PlayerNumber.P1,
+                FromName = "Testistan",
+                ToName = "Testanbul"
+            }));
+            Assert.IsFalse(actions.Contains(new Attack()
+            {
+                PlayerNumber = PlayerNumber.P1,
+                FromName = "Testistan",
+                ToName = "Testlyvania"
+            }));
+        }
+
+        [TestMethod]
+        public void Moves()
+        {
+            Game game = new GameTest().MakeExampleGame();
+
+            game.Players[PlayerNumber.P1].UnitsToMove = 1;
+            var actions = game.Countries["Testistan"].Moves();
+            Assert.IsFalse(actions.Contains(new Move()
+            {
+                PlayerNumber = PlayerNumber.P1,
+                FromName = "Testistan",
+                ToName = "Testanbul"
+            }));
+            Assert.IsTrue(actions.Contains(new Move()
+            {
+                PlayerNumber = PlayerNumber.P1,
+                FromName = "Testistan",
+                ToName = "Testlyvania"
+            }));
+        }
+
+        [TestMethod]
+        public void Moves_Out()
+        {
+            Game game = new GameTest().MakeExampleGame();
+
+            game.Players[PlayerNumber.P1].UnitsToMove = 0;
+            var actions = game.Countries["Testistan"].Moves();
+            Assert.IsFalse(actions.Contains(new Move()
+            {
+                PlayerNumber = PlayerNumber.P1,
+                FromName = "Testistan",
+                ToName = "Testanbul"
+            }));
+            Assert.IsFalse(actions.Contains(new Move()
+            {
+                PlayerNumber = PlayerNumber.P1,
+                FromName = "Testistan",
+                ToName = "Testlyvania"
+            }));
+        }
+    }
+}
---
+++ CS3450.TooRisky.Tests/Model/GameTest.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using CS3450.TooRisky.Model;
+
+namespace CS3450.TooRisky.Tests.Model
+{
+    [TestClass]
+    public class GameTest
+    {
+        public Game MakeExampleGame()
+        {
+            var game = Game.DisposeAndAcreateNewGame();
+
+            game.AddPlayer(new Player()
+            {
+                //PlayerNumber = PlayerNumber.P1,
+                Name = "Player One",
+                Color = 0xFFFF0000,
+            });
+
+            game.AddPlayer(new Player()
+            {
+                //PlayerNumber = PlayerNumber.P2,
+                Name = "Player Two",
+                Color = 0xFF0000FF,
+            });
+
+
+
+            Country country;
+
+            country = new Country()
+            {
+                Name = "Testistan",
+                Units = 1,
+                X = 0,
+                Y = 0,
+                OwnedBy = PlayerNumber.P1
+            };
+            country.AdjacentCountryNames.Add("Testlyvania");
+            country.AdjacentCountryNames.Add("Testanbul");
+            game.AddCountry(country);
+
+            country = new Country()
+            {
+                Name = "Testlyvania",
+                Units = 1,
+                X = 10,
+                Y = 0,
+                OwnedBy = PlayerNumber.P1
+            };
+            country.AdjacentCountryNames.Add("Testistan");
+            country.AdjacentCountryNames.Add("Testanbul");
+            game.AddCountry(country);
+
+            country = new Country()
+            {
+                Name = "Testanbul",
+                Units = 1,
+                X = 0,
+                Y = 10,
+                OwnedBy = PlayerNumber.P2
+            };
+            country.AdjacentCountryNames.Add("Testistan");
+            country.AdjacentCountryNames.Add("Testlyvania");
+            country.AdjacentCountryNames.Add("Test States of America");
+            game.AddCountry(country);
+
+            country = new Country()
+            {
+                Name = "Test States of America",
+                Units = 1,
+                X = 0,
+                Y = 20,
+                OwnedBy = PlayerNumber.P1
+            };
+            country.AdjacentCountryNames.Add("Testanbul");
+            game.AddCountry(country);
+
+
+
+            Continent continent;
+            continent = new Continent()
+            {
+                Name = "Testartica",
+                Worth = 3,
+            };
+            continent.CountryNames.Add("Testistan");
+            continent.CountryNames.Add("Testlyvania");
+            game.AddContinent(continent);
+
+            continent = new Continent()
+            {
+                Name = "Testarica",
+                Worth = 3,
+            };
+            continent.CountryNames.Add("Testanbul");
+            continent.CountryNames.Add("Test States of America");
+            game.AddContinent(continent);
+
+            return game;
+        }
+    }
+}
